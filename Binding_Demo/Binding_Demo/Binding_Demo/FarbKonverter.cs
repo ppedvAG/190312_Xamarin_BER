85195ca32c8f2b3b8af86c8bc6ba5562d9711e07
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-13 10:03:40 +0100

    Binding und ValueConverter

---
+++ Binding_Demo/Binding_Demo/Binding_Demo/FarbKonverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Binding_Demo
+{
+    class FarbKonverter : IValueConverter
+    {
+        // OneWay
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            #region Easy-Version
+            //string text = (string)value;
+            //if (text == "Red")
+            //    return Color.Red;
+            //else if (text == "Green")
+            //    return Color.Green;
+            //else
+            //    return Color.White; 
+            #endregion
+
+            string text = (string)value;
+            var field = typeof(Color).GetFields().FirstOrDefault(x => x.Name.ToLower() == text.ToLower());
+
+            if (field != null)
+                return field.GetValue(typeof(Color));
+            else
+                return Color.White;
+            
+        }
+
+        // TwoWay
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
