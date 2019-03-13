388935ab4995aa380c0010eaac2a1eca2da81fbc
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-13 13:50:33 +0100

    Images

--- Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
+++ Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
@@ -12,6 +12,17 @@ namespace Image_Demo
         public MainPage()
         {
             InitializeComponent();
+
+            // Caching für heruntergeladene Bilder steuern
+            //UriImageSource source = (UriImageSource)ImageSource.FromUri(new Uri("https://www.zooroyal.de/magazin/wp-content/uploads/2017/11/Goldhamster-760x560.jpg"));
+            //source.CachingEnabled = true; // true ist bereits standardfall !
+            //source.CacheValidity = TimeSpan.FromHours(1);
+            //imageDemo.Source = source;
+
+            // imageDemo.Source = ImageSource.FromFile("hamster.jpg");
+
+            // RessurcenID: Projektname.Ordner.Dateiname.Extension
+            // imageDemo.Source = ImageSource.FromResource("Image_Demo.Images.hamster.jpg");
         }
     }
 }
