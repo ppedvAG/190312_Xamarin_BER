f9a6b2db04173af07ccca4aa53b4be049ff1e60f
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-03-15 09:36:32 +0100

    Umbau auf TabbedPage

---
+++ XamarinPlugins/XamarinPlugins/XamarinPlugins/PropertiesPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinPlugins
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PropertiesPage : ContentPage
+    {
+        public PropertiesPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            //var result  = await TextToSpeech.GetLocalesAsync();
+            //listViewLocales.ItemsSource = result.ToList();
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
